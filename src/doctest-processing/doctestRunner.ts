118c8dddb2c3bdcb997bb6922bbaf4a4a1ff205a
Diego Imbert <diegoimbert@pm.me>
2023-11-29 17:26:42 +0100

    print as json

--- src/doctest-processing/doctestRunner.ts
+++ src/doctest-processing/doctestRunner.ts
@@ -10,9 +10,15 @@ export function runAllTests(path: string) {
   return `{if (process.argv.includes('${path}')) {__doctests__.forEach(${d} => ${runDoctest})}}`
 }
 
-const logJson = (json: string) => `console.log("${DOCTEST_RESULT_LOG_ID}", ${json});`
+const logJson = (json: string) => `console.log("${DOCTEST_RESULT_LOG_ID} " + JSON.stringify(${json}));`
+
 const okMsg = `{status: 'ok', line: ${d}.line}`
 const errMsg = `{status: 'err', line: ${d}.line, msg: ${e}.message}`
 
 const runDoctest =
   `{ try { ${d}.f();${logJson(okMsg)}; } catch(${e}) { ${logJson(errMsg)} } }`
+
+export type TestResult = { line: number } & (
+  | { status: 'err', msg: string }
+  | { status: 'ok' }
+)
