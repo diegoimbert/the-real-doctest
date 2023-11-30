75a8bdbc2a2c4df179c70c1377a68849d36e1b40
Diego Imbert <diegoimbert@pm.me>
2023-11-30 13:13:43 +0100

    Custom run command

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { array, command, multioption, number, oneOf, option, positional, run } from 'cmd-ts';
+import { array, command, multioption, number, oneOf, option, positional, run, string } from 'cmd-ts';
 import { File } from 'cmd-ts/batteries/fs';
 import { readFile } from 'node:fs/promises';
 import { transformFileWithDoctests } from './doctest-processing/sourceComponents/fileRunner';
@@ -8,29 +8,36 @@ import { execSync } from 'node:child_process';
 import { DOCTEST_RESULT_LOG_ID, DoctestResult } from './doctest-processing/sourceComponents/doctestRunner';
 import { doctestOutputFormat, getFormatter } from './doctest-processing/formatter';
 
-const cmd = command({
+const args = {
+  lines: multioption({
+    long: 'line',
+    short: 'l',
+    type: array(number),
+    description: "Doctest lines to be executed. Executes all if none specified"
+  }),
+  path: positional({
+    type: File,
+    displayName: 'path',
+    description: "The file that contains the doctests to be run"
+  }),
+  outputFormat: option({
+    long: 'output-format',
+    defaultValue: () => "cli" as const,
+    type: oneOf(doctestOutputFormat)
+  }),
+  runCommand: option({
+    long: "run-command",
+    type: string,
+    defaultValue: () => "node --import the-real-doctest --import tsx"
+  })
+}
+
+const cli = command({
   name: 'The Real Doctest',
   description: 'Execute TSDoc examples',
   version: '0.0.1',
-  args: {
-    lines: multioption({
-      long: 'line',
-      short: 'l',
-      type: array(number),
-      description: "Doctest lines to be executed. Executes all if none specified"
-    }),
-    path: positional({
-      type: File,
-      displayName: 'path',
-      description: "The file that contains the doctests to be run"
-    }),
-    outputFormat: option({
-      long: 'output-format',
-      defaultValue: () => "cli" as const,
-      type: oneOf(doctestOutputFormat)
-    }),
-  },
-  async handler({ lines, path, outputFormat }) {
+  args,
+  async handler({ lines, path, outputFormat, runCommand }) {
     const doctestTempPath = generateTempPath(path, '.doctest.tmp')
     await withTempFile({
       path: doctestTempPath,
@@ -40,8 +47,7 @@ const cmd = command({
         ...lines.length && { filter: (d) => lines.includes(d.lineNumber + 1) },
       }),
       async fn() {
-        const cmd = `node --import the-real-doctest --import tsx ${doctestTempPath}`
-        const rawResult = execSync(cmd).toString()
+        const rawResult = execSync(`${runCommand} ${doctestTempPath}`).toString()
         const result = rawResult.split('\n')
           .filter(line => line.startsWith(DOCTEST_RESULT_LOG_ID))
           .map(line => line.substring(DOCTEST_RESULT_LOG_ID.length))
@@ -51,4 +57,4 @@ const cmd = command({
     })
   },
 });
-run(cmd, argv.slice(2))
+run(cli, argv.slice(2))
