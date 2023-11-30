51893eea316290e585a8672d6cdbfd93dc4f20a8
Diego Imbert <diegoimbert@pm.me>
2023-11-30 11:25:01 +0100

    started CLI

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,34 @@
+import { array, command, multioption, number, positional, run, string } from 'cmd-ts';
+import { File } from 'cmd-ts/batteries/fs';
 import { readFile } from 'node:fs/promises';
+import { transformFileWithDoctests } from './doctest-processing/sourceComponents/fileRunner';
+import { argv } from 'node:process';
+import { generateTempPath, withTempFile } from './doctest-processing/withTempFile';
 
-export async function main(path: string) {
-  const content = (await readFile(path)).toString()
-  
-  return source
-}
+const cmd = command({
+  name: 'The Real Doctest',
+  description: 'Execute TSDoc examples',
+  version: '1.0.0',
+  args: {
+    lines: multioption({
+      long: 'line',
+      short: 'l',
+      type: array(number),
+    }),
+    path: positional({ type: File, displayName: 'path' }),
+  },
+  async handler({lines, path}) {
+    await withTempFile({
+      path: generateTempPath(path, '.doctest.tmp'),
+      content: transformFileWithDoctests({
+        content: (await readFile(path)).toString(),
+        path,
+        ...lines.length && { filter: (d) => lines.includes(d.lineNumber + 1) },
+      }),
+      async fn() {
+        
+      }
+    })
+  },
+});
+run(cmd, argv.slice(2))
