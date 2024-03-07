95b4c857787601dc5f9ce3677d46ae7928da174f
Diego Imbert <diegoimbert@pm.me>
2024-03-07 17:10:17 +0100

    get rid of all direct package access

--- src/lib/cli/commands/install.ts
+++ src/lib/cli/commands/install.ts
@@ -4,56 +4,55 @@
 // https://opensource.org/licenses/MIT
 
 import { command } from "cmd-ts"
-import { withLib } from "../withLib"
 import fs from "fs/promises"
 import cliSelect from "cli-select"
 import { exec } from "node:child_process"
+import doctestTsConfig from "../../../../tsconfig.doctest.json"
 
 export const installCommand = command({
   name: "Installer",
   description: "Installs the-real-doctest default environment in the current project",
   args: {},
   handler: async args => {
-    withLib({
-      forceCopy: false,
-      handler: async ({ rootPath }) => {
-        // Copy testing tsconfig
-        await fs.cp(`${rootPath}/dist/tsconfig.doctest.json`, "./tsconfig.doctest.json")
-        console.log(`Copied ./tsconfig.doctest.json`)
+    // Copy testing tsconfig
+    await fs.writeFile(
+      "./tsconfig.doctest.json",
+      JSON.stringify(doctestTsConfig, null, 2),
+      "utf8"
+    )
+    console.log(`Wrote ./tsconfig.doctest.json`)
 
-        // Install dev dependencies
-        console.log("Select your project's package manager:")
-        const { value: packageManager } = await cliSelect({
-          values: ["npm", "yarn", "pnpm", "bun"] as const,
-          indentation: 2
-        })
-
-        let installDevSubCmd: Record<typeof packageManager, string> = {
-          bun: "add -d",
-          pnpm: "add -D",
-          npm: "install -D",
-          yarn: "add -D"
-        }
-        let installCmd = `${packageManager} ${installDevSubCmd[packageManager]} ts-node ts-patch`
-
-        console.log(`Running ${installCmd}`)
-        await new Promise((resolve, reject) => {
-          exec(installCmd, (error, stdout, stderr) => {
-            console.log(stderr)
-            resolve(null)
-          })
-        })
-        console.log("OK")
-
-        // Put default test command in package.json
-        console.log("Writing default config in package.json")
-        const packageJson = JSON.parse(await fs.readFile("./package.json", "utf8"))
-        packageJson["the-real-doctest"] = {
-          runCommand:
-            "npx ts-node --project tsconfig.doctest.json --compiler ts-patch/compiler"
-        }
-        await fs.writeFile("./package.json", JSON.stringify(packageJson, null, 2))
-      }
+    // Install dev dependencies
+    console.log("Select your project's package manager:")
+    const { value: packageManager } = await cliSelect({
+      values: ["npm", "yarn", "pnpm", "bun"] as const,
+      indentation: 2
     })
+
+    let installDevSubCmd: Record<typeof packageManager, string> = {
+      bun: "add -d",
+      pnpm: "add -D",
+      npm: "install -D",
+      yarn: "add -D"
+    }
+    let installCmd = `${packageManager} ${installDevSubCmd[packageManager]} ts-node ts-patch`
+
+    console.log(`Running ${installCmd}`)
+    await new Promise((resolve, reject) => {
+      exec(installCmd, (error, stdout, stderr) => {
+        console.log(stderr)
+        resolve(null)
+      })
+    })
+    console.log("OK")
+
+    // Put default test command in package.json
+    console.log("Writing default config in package.json")
+    const packageJson = JSON.parse(await fs.readFile("./package.json", "utf8"))
+    packageJson["the-real-doctest"] = {
+      runCommand:
+        "npx ts-node --project tsconfig.doctest.json --compiler ts-patch/compiler"
+    }
+    await fs.writeFile("./package.json", JSON.stringify(packageJson, null, 2))
   }
 })
--- src/lib/cli/commands/test.ts
+++ src/lib/cli/commands/test.ts
@@ -4,7 +4,6 @@
 // https://opensource.org/licenses/MIT
 
 import { command, positional } from "cmd-ts"
-import { withLib } from "../withLib"
 import { exec } from "node:child_process"
 import fs from "fs/promises"
 import { File } from "cmd-ts/batteries/fs"
@@ -20,50 +19,45 @@ export const testCommand = command({
     file: positional(File)
   },
   handler: async args => {
-    withLib({
-      forceCopy: false,
-      handler: async ({ rootPath }) => {
-        await new Promise(async (resolve, reject) => {
-          const packageJson = JSON.parse(await fs.readFile("./package.json", "utf8"))
-          const runCmd = packageJson?.["the-real-doctest"]?.["runCommand"]
-          if (!runCmd || typeof runCmd != "string") {
-            console.log(
-              "Could not find run command in package.json. Do you want to run 'npx the-real-doctest install'?"
-            )
-            const { value: shouldRunInstall } = await cliSelect({
-              values: ["Yes", "No"] as const,
-              indentation: 2
-            })
-            if (shouldRunInstall !== "Yes") {
-              return
-            }
-            await installCommand.handler({})
-          }
-          const cmd = `${runCmd} ${args.file}`
-          exec(cmd, (error, stdout, stderr) => {
-            console.log(stderr)
-            const testResults = stdout
-              .split("\n")
-              .filter(l => l.startsWith(outputPrefix))
-              .map(l => l.substring(outputPrefix.length))
-              .map(l => JSON.parse(l) as RawResult)
-            const succeeded = testResults.filter(test => test.type == "success")
-            const failed = testResults.filter(test => test.type == "error")
-            if (succeeded.length) {
-              console.log(`${succeeded.length} doctests succeeded`)
-            }
-            if (failed.length) {
-              console.log(`${failed.length} doctests failed`)
-              failed.forEach(test => {
-                if (test.type !== "error") return // Type narrowing doesn't work on that yet?
-                console.log(`At ${test.path}`)
-                console.log(` >> ${test.message}`)
-              })
-            }
-            resolve(null)
-          })
+    await new Promise(async (resolve, reject) => {
+      const packageJson = JSON.parse(await fs.readFile("./package.json", "utf8"))
+      const runCmd = packageJson?.["the-real-doctest"]?.["runCommand"]
+      if (!runCmd || typeof runCmd != "string") {
+        console.log(
+          "Could not find run command in package.json. Do you want to run 'npx the-real-doctest install'?"
+        )
+        const { value: shouldRunInstall } = await cliSelect({
+          values: ["Yes", "No"] as const,
+          indentation: 2
         })
+        if (shouldRunInstall !== "Yes") {
+          return
+        }
+        await installCommand.handler({})
       }
+      const cmd = `${runCmd} ${args.file}`
+      exec(cmd, (error, stdout, stderr) => {
+        console.log(stderr)
+        const testResults = stdout
+          .split("\n")
+          .filter(l => l.startsWith(outputPrefix))
+          .map(l => l.substring(outputPrefix.length))
+          .map(l => JSON.parse(l) as RawResult)
+        const succeeded = testResults.filter(test => test.type == "success")
+        const failed = testResults.filter(test => test.type == "error")
+        if (succeeded.length) {
+          console.log(`${succeeded.length} doctests succeeded`)
+        }
+        if (failed.length) {
+          console.log(`${failed.length} doctests failed`)
+          failed.forEach(test => {
+            if (test.type !== "error") return // Type narrowing doesn't work on that yet?
+            console.log(`At ${test.path}`)
+            console.log(` >> ${test.message}`)
+          })
+        }
+        resolve(null)
+      })
     })
   }
 })
